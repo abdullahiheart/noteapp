84bf44ba33193420fd28e60d6e0e1937afffceb4
abdullahi Hear <abdalaheart@gmail.com>
2023-05-19 14:33:14 +0300

    remove hinttextstyle

--- lib/views/custom_text_field.dart
+++ lib/views/custom_text_field.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:noteapp/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.hint,  this.maxLines=1});
+  const CustomTextField({super.key, required this.hint, this.maxLines = 1});
   final String hint;
   final int maxLines;
   // get color => null;
@@ -13,11 +13,10 @@ class CustomTextField extends StatelessWidget {
       cursorColor: kPrimaryColor,
       maxLines: maxLines,
       decoration: InputDecoration(
-        
         hintText: hint,
-        hintStyle: TextStyle(
-          color: kPrimaryColor,
-        ),
+        // hintStyle: TextStyle(
+        //   color: kPrimaryColor,
+        // ),
         border: buildBorder(),
         enabledBorder: buildBorder(),
         focusedBorder: buildBorder(kPrimaryColor),
