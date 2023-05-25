061ef9d545fe08d3d73d8d76cc0129b3282e10bd
abdullahi Hear <abdalaheart@gmail.com>
2023-05-25 13:26:00 +0300

    register adapter

---
+++ lib/cubits/add_note_state.dart
@@ -0,0 +1,18 @@
+// import 'package:flutter/material.dart';
+
+part of 'add_note_cubit.dart';
+
+@immutable
+abstract class AddNoteState {}
+
+class AddNoteInitial extends AddNoteState {}
+
+class AddNoteLoading extends AddNoteState {}
+
+class AddNoteSuccess extends AddNoteState {}
+
+class AddNoteFailure extends AddNoteState {
+  final String errMessage;
+
+  AddNoteFailure(this.errMessage);
+}
